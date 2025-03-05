682ee3bd0350447d775fdaaa15300a30e6ea03d4
Ivaz <ivazreza@gmail.com>
2025-03-05 09:16:03 +0700

    makes left side dropdown have transisition

--- src/pages/home/homeScreen/homeScreen/header/leftSideHeaderHome.jsx
+++ src/pages/home/homeScreen/homeScreen/header/leftSideHeaderHome.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 import SpanTriangle from "../../../../../components/spanTriagle";
+import { motion, AnimatePresence } from "framer-motion";
 
 const LeftSideHeaderHome = () => {
   const [isHover, setIsHover] = useState(false);
@@ -15,29 +16,39 @@ const LeftSideHeaderHome = () => {
   const HandleMouseLeave = () => {
     timeOutRef.current = setTimeout(() => {
       setIsHover(false)
-    }, 450)
+    }, 250)
   }
 
   return (
     <div className="headerHome__inside__leftSide d-flex flex-row gap-2 align-items-center">
       <img className="netflix-home" src="/netflix-logo.png" alt="logoNetflix" />
-
+      
       <div onMouseEnter={HandleMouseEnter} onMouseLeave={HandleMouseLeave} className="headerHome__inside__leftSide__dropDown d-flex align-items-center gap-2">
         Browse <SpanTriangle rotated={true} color={"rgb(230,230,230)"}/>
 
-        {isHover && <div className="headerHome__inside__leftSide__dropDown__itemDropDown d-flex flex-column align-items-center gap-1">
-          <SpanTriangle color={"rgb(230,230,230)"} />
+        <AnimatePresence>
+          {isHover && 
+            <motion.div 
+              initial={{ opacity: 1 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.25 }}
+              className="headerHome__inside__leftSide__dropDown__itemDropDown d-flex flex-column align-items-center gap-1"
+            >
+              <SpanTriangle color={"rgb(230,230,230)"} />
 
-          <div className="headerHome__inside__leftSide__dropDown__itemDropDown__btnContainer d-flex flex-column">
-            <button>Home</button>
+              <div className="headerHome__inside__leftSide__dropDown__itemDropDown__btnContainer d-flex flex-column">
+                <button>Home</button>
 
-            <button>Movies</button>
+                <button>Movies</button>
 
-            <button>Tv Shows</button>
+                <button>Tv Shows</button>
 
-            <button>My List</button>
-          </div>
-        </div>}
+                <button>My List</button>
+              </div>
+            </motion.div>
+          }
+        </AnimatePresence>
       </div>
 
       <div className="headerHome__inside__leftSide__themeSelected d-flex flex-row gap-2 gap-xl-3">
