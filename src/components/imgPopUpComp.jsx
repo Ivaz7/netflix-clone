1907d74d0b8621486d828e3c8f338cf37ba514a9
Ivaz <ivazreza@gmail.com>
2025-03-11 11:15:47 +0700

    makes buttonRating have logic to check to database now

--- src/components/imgPopUpComp.jsx
+++ src/components/imgPopUpComp.jsx
@@ -1,13 +1,39 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { genreMap } from "../data/movieGenreData";
+import { useClickOutside } from "../customHooks/useClickOutside";
+import { useGetDataQuery } from "../service/redux/API/firebaseDB";
+import LoadingComp from "./loadingComp";
 
 const ImgPopUpComp = ({ data }) => {
-  const { poster_path, genre_ids } = data;
+  const { poster_path, genre_ids, id: idMovie } = data;
   
-  const [ratingIndex, setRatingIndex] = useState(1);
+  const [ratingIndex, setRatingIndex] = useState(null);
   const [isRating, setIsRating] = useState(false);
+  const optionRatingRef = useRef(null);
+  const timeOutRatingRef = useRef(null);
+  const clickOutSide = useClickOutside;
+
+  const { data: dataGet, isLoading } = useGetDataQuery();
+
+  const userOptionSelected = dataGet?.userOption[dataGet.userSelected];
+  const { rating } = userOptionSelected.history;
+
+  const rated =
+    rating !== "empty" && Array.isArray(rating)
+      ? rating.find((val) => val.id === idMovie) || null
+      : null;
+
+  clickOutSide(optionRatingRef, isRating, setIsRating);
+
+  useEffect(() => {
+    if (rated && typeof rated.score === "number") {
+      setRatingIndex(rated.score);
+    } else {
+      setRatingIndex(null);
+    }
+  }, [rated]);
 
   const renderGenre = genre_ids.map((val, index) => {
     return <p key={index}>{genreMap[val]}</p>;
@@ -16,9 +42,48 @@ const ImgPopUpComp = ({ data }) => {
   const buttonIcons = ["fa-thumbs-down", "fa-thumbs-up", "fa-heart"];
 
   const handleOptionRating = (idx) => {
+    if (ratingIndex === idx) {
+      setRatingIndex(null);
+    } else {
+      setRatingIndex(idx);
+    }
     setIsRating(false);
-    setRatingIndex(idx);
   };
+
+  const renderSelectedRating = (
+    <button onMouseEnter={() => setIsRating(true)} onClick={() => setIsRating(true)}>
+      <i
+        className={`fa-${
+          ratingIndex !== null ? "solid" : "regular"
+        } ${ratingIndex !== null ? buttonIcons[ratingIndex] : "fa-thumbs-up"}`}
+      ></i>
+    </button>
+  );
+
+  const renderRatingOption = buttonIcons.map((icon, idx) => {
+    return (
+      <button key={idx} onClick={() => handleOptionRating(idx)}>
+        <i className={`fa-${ratingIndex === idx ? "solid" : "regular"} ${icon}`}></i>
+      </button>
+    );
+  });
+
+  const handleLeaveRating = () => {
+    timeOutRatingRef.current = setTimeout(() => {
+      setIsRating(false);
+    }, 250);
+  };
+
+  const handleEnterRating = () => {
+    if (timeOutRatingRef.current) {
+      clearTimeout(timeOutRatingRef.current);
+    }
+    setIsRating(true);
+  };
+
+  if (isLoading) {
+    return <LoadingComp />;
+  }
 
   return (
     <div className="ImgPopUpComp">
@@ -37,43 +102,43 @@ const ImgPopUpComp = ({ data }) => {
 
         <div className="ImgPopUpComp__popUp__optionBtn d-flex flex-row align-items-center justify-content-between">
           <div className="d-flex flex-row gap-1">
-            <button>
+            <button className="buttonPlay buttonOutside">
               <i className="fa-solid fa-play"></i>
             </button>
 
-            <button>
-              <i className="fa-solid fa-check"></i>
+            <button className="buttonList buttonOutside">
+              {/* <i className="fa-solid fa-check"></i> */}
+              <i className="fa-solid fa-plus"></i>
             </button>
 
-            <div className="buttonRating">
-              <button onClick={() => setIsRating(true)}>
-                <i className={`fa-${ratingIndex !== null ? "solid" : "regular"} ${buttonIcons[ratingIndex]}`}></i>
-              </button>
+            <div className="buttonRating buttonOutside">
+              {renderSelectedRating}
 
               <AnimatePresence>
-                {isRating && <motion.div 
-                  className="buttonRating__ratingOption d-flex flex-row gap-1"
-                  initial={{ scale: 0.8 }}
-                  animate={{ scale: 1 }}
-                  exit={{ scale: 0.7 }}
-                  transition={{ duration: 0.25 }}
-                >
-                  {buttonIcons.map((icon, idx) => (
-                    <button key={idx} onClick={() => handleOptionRating(idx)}>
-                      <i className={`fa-${ratingIndex === idx ? "solid" : "regular"} ${icon}`}></i>
-                    </button>
-                  ))}
-                </motion.div>}
+                {isRating && (
+                  <motion.div
+                    className="buttonRating__ratingOption d-flex flex-row gap-1"
+                    initial={{ scale: 0.8 }}
+                    animate={{ scale: 1 }}
+                    exit={{ scale: 0.7 }}
+                    transition={{ duration: 0.25 }}
+                    ref={optionRatingRef}
+                    onMouseLeave={handleLeaveRating}
+                    onMouseEnter={handleEnterRating}
+                  >
+                    {renderRatingOption}
+                  </motion.div>
+                )}
               </AnimatePresence>
             </div>
           </div>
 
-          <button>
+          <button className="buttonMoreInfo buttonOutside">
             <i className="fa-solid fa-chevron-down"></i>
           </button>
         </div>
 
-        <div className="ImgPopUpComp__popUp__information">
+        <div className="ImgPopUpComp__popUp__information d-flex flex-row gap-2 flex-wrap">
           {renderGenre}
         </div>
       </div>
