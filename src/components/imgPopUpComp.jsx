170a3eabc405f4ed0cff8630aa070c1efc19feb4
Ivaz <ivazreza@gmail.com>
2025-03-11 11:49:26 +0700

    makes popUp from ImgPopUpComp now interactive

--- src/components/imgPopUpComp.jsx
+++ src/components/imgPopUpComp.jsx
@@ -11,8 +11,11 @@ const ImgPopUpComp = ({ data }) => {
   
   const [ratingIndex, setRatingIndex] = useState(null);
   const [isRating, setIsRating] = useState(false);
+  const [isHover, setIsHover] = useState(false);
   const optionRatingRef = useRef(null);
   const timeOutRatingRef = useRef(null);
+  const mainPopUpRef = useRef(null);
+  const timeOutMainPopUp = useRef(null);
   const clickOutSide = useClickOutside;
 
   const { data: dataGet, isLoading } = useGetDataQuery();
@@ -81,6 +84,20 @@ const ImgPopUpComp = ({ data }) => {
     setIsRating(true);
   };
 
+  const handleEnterPopUp = () => {
+    if (timeOutMainPopUp.current) {
+      clearTimeout(timeOutMainPopUp.current);
+    }
+    setIsHover(true);
+  };
+  
+  const handleLeavePopUP = () => {
+    timeOutMainPopUp.current = setTimeout(() => {
+      setIsHover(false);
+    }, 250);
+  };
+  
+
   if (isLoading) {
     return <LoadingComp />;
   }
@@ -88,60 +105,72 @@ const ImgPopUpComp = ({ data }) => {
   return (
     <div className="ImgPopUpComp">
       <img
+        onMouseEnter={handleEnterPopUp}
         className="ImgPopUpComp__mainImg"
         src={`https://image.tmdb.org/t/p/original${poster_path}`}
         alt="background"
       />
 
-      <div className="ImgPopUpComp__popUp">
-        <img
-          className="ImgPopUpComp__popUp__img"
-          src={`https://image.tmdb.org/t/p/original${poster_path}`}
-          alt="background"
-        />
+      <AnimatePresence>
+        {isHover && <motion.div 
+          className="ImgPopUpComp__popUp"
+          ref={mainPopUpRef}
+          initial={{ scale: 1 }}
+          animate={{ scale: 1.1 }}
+          exit={{ scale: 1  }}
+          transition={{ duration: 0.25 }}
+          onMouseEnter={handleEnterPopUp}
+          onMouseLeave={handleLeavePopUP}
+        >
+          <img
+            className="ImgPopUpComp__popUp__img"
+            src={`https://image.tmdb.org/t/p/original${poster_path}`}
+            alt="background"
+          />
 
-        <div className="ImgPopUpComp__popUp__optionBtn d-flex flex-row align-items-center justify-content-between">
-          <div className="d-flex flex-row gap-1">
-            <button className="buttonPlay buttonOutside">
-              <i className="fa-solid fa-play"></i>
-            </button>
+          <div className="ImgPopUpComp__popUp__optionBtn d-flex flex-row align-items-center justify-content-between">
+            <div className="d-flex flex-row gap-1">
+              <button className="buttonPlay buttonOutside">
+                <i className="fa-solid fa-play"></i>
+              </button>
 
-            <button className="buttonList buttonOutside">
-              {/* <i className="fa-solid fa-check"></i> */}
-              <i className="fa-solid fa-plus"></i>
-            </button>
+              <button className="buttonList buttonOutside">
+                {/* <i className="fa-solid fa-check"></i> */}
+                <i className="fa-solid fa-plus"></i>
+              </button>
 
-            <div className="buttonRating buttonOutside">
-              {renderSelectedRating}
+              <div className="buttonRating buttonOutside">
+                {renderSelectedRating}
 
-              <AnimatePresence>
-                {isRating && (
-                  <motion.div
-                    className="buttonRating__ratingOption d-flex flex-row gap-1"
-                    initial={{ scale: 0.8 }}
-                    animate={{ scale: 1 }}
-                    exit={{ scale: 0.7 }}
-                    transition={{ duration: 0.25 }}
-                    ref={optionRatingRef}
-                    onMouseLeave={handleLeaveRating}
-                    onMouseEnter={handleEnterRating}
-                  >
-                    {renderRatingOption}
-                  </motion.div>
-                )}
-              </AnimatePresence>
+                <AnimatePresence>
+                  {isRating && (
+                    <motion.div
+                      className="buttonRating__ratingOption d-flex flex-row gap-1"
+                      initial={{ scale: 0.8 }}
+                      animate={{ scale: 1 }}
+                      exit={{ scale: 0.7 }}
+                      transition={{ duration: 0.25 }}
+                      ref={optionRatingRef}
+                      onMouseLeave={handleLeaveRating}
+                      onMouseEnter={handleEnterRating}
+                    >
+                      {renderRatingOption}
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </div>
             </div>
+
+            <button className="buttonMoreInfo buttonOutside">
+              <i className="fa-solid fa-chevron-down"></i>
+            </button>
           </div>
 
-          <button className="buttonMoreInfo buttonOutside">
-            <i className="fa-solid fa-chevron-down"></i>
-          </button>
-        </div>
-
-        <div className="ImgPopUpComp__popUp__information d-flex flex-row gap-2 flex-wrap">
-          {renderGenre}
-        </div>
-      </div>
+          <div className="ImgPopUpComp__popUp__information d-flex flex-row gap-2 flex-wrap">
+            {renderGenre}
+          </div>
+        </motion.div>}
+      </AnimatePresence>
     </div>
   );
 };
