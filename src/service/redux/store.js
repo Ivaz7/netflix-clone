6ce3d4f9f7b00d8dc10e6277fee5f59624043d01
Ivaz <ivazreza@gmail.com>
2025-03-13 16:28:32 +0700

    deleting some slice that is unnecessary

--- src/service/redux/store.js
+++ src/service/redux/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { tmdbApiSlice } from "./API/tmdbApiSlicee.js";
-import moviaDataReducer from "./slice/movieData.js";
 import QandAReducer from "./slice/QandAData.js";
 import signUpEmailReducer from "./slice/signUpEmailSlice.js";
 import { fireBaseAuthSlice } from "./API/fireBaseAuthSlice.js";
@@ -10,7 +9,6 @@ import profileEditReducer from "./API/profileEditSlice.js";
 export const store = configureStore({
   reducer: {
     [tmdbApiSlice.reducerPath]: tmdbApiSlice.reducer,
-    movieData: moviaDataReducer,
     QandAData: QandAReducer,
     signUpEmail: signUpEmailReducer,
     [fireBaseAuthSlice.reducerPath]: fireBaseAuthSlice.reducer,
