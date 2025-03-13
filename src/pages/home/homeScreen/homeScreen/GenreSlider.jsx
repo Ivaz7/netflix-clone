eb4f3e4086a768a15bcffc5e2b88ebed8cb7ff28
Ivaz <ivazreza@gmail.com>
2025-03-13 12:45:57 +0700

    finishing the homeScreen page but still having some z index probelm

---
+++ src/pages/home/homeScreen/homeScreen/GenreSlider.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useMemo, useState } from "react";
+import LoadingComp from "../../../../components/loadingComp";
+import { useGetDataQuery } from "../../../../service/redux/API/firebaseDB";
+import { useSearchParams } from "react-router-dom";
+import ImgPopUpComp from "../../../../components/imgPopUpComp";
+import MainSlider from "../../../../components/mainSlider";
+import PropTypes from "prop-types";
+
+const GenreSlider = ({ name, fetchMovie, fetchTV }) => {
+  const { data: dataGet, isLoading: loadingDataGet } = useGetDataQuery();
+  const [triggerMovie, { data: dataMovie, isLoading: loadingMovie }] = fetchMovie();
+  const [triggerTv, { data: dataTv, isLoading: loadingTv }] = fetchTV();
+  const [resultsArr, setResultsArr] = useState([]);
+  const [searchParams] = useSearchParams();
+  const category = searchParams.get("category");
+
+  const userOptionSelected = dataGet?.userOption[dataGet?.userSelected];
+  const statusAge = userOptionSelected?.statusAge;
+
+  useEffect(() => {
+    triggerMovie({ age: statusAge });
+    triggerTv({ age: statusAge });
+  }, [triggerMovie, triggerTv, statusAge]);
+
+  const resultsTv = useMemo(() => (dataTv?.results || []), [dataTv]);
+  const resultsMovie = useMemo(() => (dataMovie?.results || []), [dataMovie]);
+
+  const resultAll = useMemo(() => {
+    return [...resultsMovie, ...resultsTv].sort(() => Math.random() - 0.5);
+  }, [resultsMovie, resultsTv]);
+
+  useEffect(() => {
+    if (category === "tv") {
+      setResultsArr(resultsTv);
+    } else if (category === "movie") {
+      setResultsArr(resultsMovie);
+    } else {
+      setResultsArr(resultAll);
+    }
+  }, [category, resultAll, resultsTv, resultsMovie]);
+
+  if (loadingDataGet || loadingTv || loadingMovie) {
+    return (
+      <div className="d-flex justify-content-center align-items-center">
+        <LoadingComp />
+      </div>
+    );
+  }
+
+  return (
+    <div className="genreSlider">
+      <MainSlider name={name}>
+        {resultsArr.map((val, inx) => (
+          <ImgPopUpComp data={val} key={inx} />
+        ))}
+      </MainSlider>
+    </div>
+  );
+};
+
+GenreSlider.propTypes = {
+  name: PropTypes.string.isRequired,
+  fetchMovie: PropTypes.func.isRequired,
+  fetchTV: PropTypes.func.isRequired,
+};
+
+export default GenreSlider;
--- src/pages/home/homeScreen/homeScreen/homeScreen.jsx
+++ src/pages/home/homeScreen/homeScreen/homeScreen.jsx
@@ -1,58 +1,26 @@
 import HeaderHome from "./header/headerHome";
 import Footer from "../../../../components/footer";
-import MainSlider from "../../../../components/mainSlider";
-import { useLazyGetActionMoviesQuery } from "../../../../service/redux/API/tmdbApiSlicee";
-import CustomFloatingComp from "../../../../components/customFloatingComp";
-import LoadingComp from "../../../../components/loadingComp";
-import ImgPopUpComp from "../../../../components/imgPopUpComp";
-import { useGetDataQuery } from "../../../../service/redux/API/firebaseDB";
-import { useEffect } from "react";
 import WelcomeBanner from "./welcomeBanner";
+import GenreSlider from "./GenreSlider";
+import { useLazyGetActionMoviesQuery, useLazyGetActionTVShowsQuery, useLazyGetComedyMoviesQuery, useLazyGetComedyTVShowsQuery, useLazyGetTrendingMoviesQuery, useLazyGetTopRatedMoviesQuery, useLazyGetPopularMoviesQuery, useLazyGetPopularTVShowsQuery, useLazyGetTopRatedTVShowsQuery, useLazyGetTrendingTVShowsQuery } from "../../../../service/redux/API/tmdbApiSlicee";
 
 const HomeScreen = () => {
-  const [triggerFetch, { data: dataMovie, isLoading: isLoadingMovie }] = useLazyGetActionMoviesQuery();
-  const { data: dataGet, isLoading: isLoadingDataGet } = useGetDataQuery();
-  
-  const userOptionSelected = dataGet?.userOption[dataGet?.userSelected];
-  const { statusAge } = userOptionSelected;
-  
-  useEffect(() => {
-    triggerFetch({ age: statusAge });
-  }, [triggerFetch, statusAge]);
-  
-  
-  if (isLoadingMovie || isLoadingDataGet) {
-    return (
-      <CustomFloatingComp>
-        <LoadingComp />
-      </CustomFloatingComp>
-    )
-  }
-
-  const results = dataMovie?.results || [];
-  const imgArray = results.map((val, index) => {
-    return (
-      <ImgPopUpComp data={val} index={index} key={index} />
-    )
-  })
-
   return (
     <div className="outerHomeScreen">
       <HeaderHome />
-
       <main className="homeScreen">
         <WelcomeBanner />
-
-        <section className="homeScreen__slider">
-          <MainSlider name={"Dummy Slide"}>
-            {imgArray}
-          </MainSlider>
+        <section className="homeScreen__slider d-flex flex-column gap-2">
+          <GenreSlider name="Popular" fetchMovie={useLazyGetPopularMoviesQuery} fetchTV={useLazyGetPopularTVShowsQuery} />
+          <GenreSlider name="Top Rated" fetchMovie={useLazyGetTopRatedMoviesQuery} fetchTV={useLazyGetTopRatedTVShowsQuery} />
+          <GenreSlider name="Trending" fetchMovie={useLazyGetTrendingMoviesQuery} fetchTV={useLazyGetTrendingTVShowsQuery} />
+          <GenreSlider name="Comedy" fetchMovie={useLazyGetComedyMoviesQuery} fetchTV={useLazyGetComedyTVShowsQuery} />
+          <GenreSlider name="Action" fetchMovie={useLazyGetActionMoviesQuery} fetchTV={useLazyGetActionTVShowsQuery} />
         </section>
-
         <Footer />
       </main>
     </div>
   );
-}
+};
 
 export default HomeScreen;
