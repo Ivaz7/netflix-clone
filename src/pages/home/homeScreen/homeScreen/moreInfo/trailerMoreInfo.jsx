df7583fa42431c0c3e2d6043412ced4771a20a6f
Ivaz <ivazreza@gmail.com>
2025-03-19 10:50:01 +0700

    makes the trailer on moreInfo now also stored in historyWatched

--- src/pages/home/homeScreen/homeScreen/moreInfo/trailerMoreInfo.jsx
+++ src/pages/home/homeScreen/homeScreen/moreInfo/trailerMoreInfo.jsx
@@ -1,12 +1,15 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSetHistoryWatchedMutation } from "../../../../../service/redux/API/firebaseDB";
 
-const TrailerMoreInfo = ({ dataTrailer }) => {
+const TrailerMoreInfo = ({ dataTrailer, dataDetail }) => {
+  const { id, name, title } = dataDetail;
   const [visibleCount, setVisibleCount] = useState(5); 
   const increment = 3; 
   const isMax = visibleCount >= dataTrailer.length;
   const navigate = useNavigate();
+  const [triggerHistoryWatched] = useSetHistoryWatchedMutation();
 
   const handleClick = () => {
     if (isMax) {
@@ -16,30 +19,31 @@ const TrailerMoreInfo = ({ dataTrailer }) => {
     }
   };
 
-  const handlePlay = (key) => {
+  const handlePlay = async (trailerName, key) => {
+    await triggerHistoryWatched({ id: id, showName: name || title, trailerName : trailerName, key: key })
     navigate(`/watch?key=${key}`)
   }
 
   const renderTrailer = dataTrailer.slice(0, visibleCount).map((val, inx) => {
-    const { name, key } = val;
+    const { name : trailerName, key } = val;
 
     return (
       <div
         className="trailerMoreInfo__trailerContainer__trailer d-flex flex-sm-row flex-column gap-2 align-items-center"
         key={inx}
-        onClick={() => handlePlay(key)}
+        onClick={() => handlePlay(trailerName, key)}
       >
         <div className="d-flex gap-sm-3 gap-2 align-items-center flex-sm-row flex-column">
           <h1>{inx + 1}</h1>
 
           <img
             src={`https://img.youtube.com/vi/${key}/maxresdefault.jpg`}
-            alt={name}
+            alt={trailerName}
             className="trailerMoreInfo__trailerContainer__trailer__thumbnail"
           />
         </div>
 
-        <h3 className="text-center flex-grow-1">{name}</h3>
+        <h3 className="text-center flex-grow-1">{trailerName}</h3>
       </div>
     );
   });
@@ -64,6 +68,7 @@ const TrailerMoreInfo = ({ dataTrailer }) => {
 
 TrailerMoreInfo.propTypes = {
   dataTrailer: PropTypes.array.isRequired,
+  dataDetail: PropTypes.object.isRequired,
 };
 
 export default TrailerMoreInfo;
