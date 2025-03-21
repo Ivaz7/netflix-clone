f8125997d05bdd26f4519f75fa72e543598e8564
Ivaz <ivazreza@gmail.com>
2025-03-21 15:37:07 +0700

    adding more infromation at the footer for more confirmation my website is safe

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,11 @@ const Footer = ({ type }) => {
   return (
     <footer className={`${type ? "footer-white" : ""} footer-child-el d-inline-flex flex-column flex-lg-row justify-content-center align-items-center gap-3`}>
       <h6 className="p-2 m-0 text-center">
-        Netflix clone for project development purposes only
+        This Netflix Clone is built solely for educational and project development purposes.  
+      </h6>
+
+      <h6 className="p-2 m-0 text-center">
+        It is not affiliated with or endorsed by Netflix in any way.
       </h6>
 
       <h6 className="p-2 m-0 text-center">
@@ -14,6 +18,7 @@ const Footer = ({ type }) => {
       <h6 className="p-2 m-0 text-center">
         Full code available on <a href="https://github.com/Ivaz7/netflix-clone">GitHub</a>
       </h6>
+
     </footer>
   )
 }
